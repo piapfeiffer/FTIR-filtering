f6ccc796a5fcf254b7bf4e9c01daef2bdfc96788
piapfeiffer <82106053+piapfeiffer@users.noreply.github.com>
2023-08-17 11:48:56 +0200

    Update PCAfilter.R

--- PCAfilter.R
+++ PCAfilter.R
@@ -15,42 +15,54 @@ PCAfilter <- function(X, threshold = 0.95){
   center <- attr(X_scaled, "scaled:center")
   scale <- attr(X_scaled, "scaled:scale")
   
+  const_cols <- apply(data_IR %>% select(starts_with("x")),2,mad)>0
+  X_scaled <- X_scaled[, const_cols]
+  center <- center[const_cols]
+  scale <- scale[const_cols]
+  attr(X_scaled, "scaled:center") <- center
+  attr(X_scaled, "scaled:scale") <- scale
+  
   # PCA on scaled and centered data
   res.PCA <- prcomp(X_scaled, center = FALSE, scale.=FALSE)
-  
-  # reconstruction
   k <- min(which(cumsum(var_explained(res.PCA))>threshold))
+    
+  # reconstruction
   Xhat <- res.PCA$x[, 1:k]%*%t(res.PCA$rotation[, 1:k])
   Xhat <- as.matrix(t((t( Xhat) * scale)) + rep(center, each = nrow(Xhat)))
 
   # computation of reconstruction error
-  RE <- matrix(data = NA, nrow= nrow(X), ncol = ncol(X))
-  colnames(RE) <- colnames(X)
+  RE <- matrix(data = NA, nrow= nrow(X), ncol = ncol(X[,const_cols]))
+  colnames(RE) <- colnames(X[,const_cols])
   for (i in 1:nrow(X)) {
-    comp <- rbind(X[i,], Xhat[i,])
+    comp <- rbind(X[i,const_cols], Xhat[i,])
     res <- t(comp[1,] - comp[2,])^2
     RE[i, ] <- res
   }
+
   MRE <- apply(RE, 2, mean)
   
   # smoothing step
   MRE_smooth <- ksmooth(1:length(MRE), MRE, "normal", bandwidth = 20)
   MRE <- MRE_smooth$y
-  threshold_non_inf <- quantile(MRE, prob = 0.95, na.rm = TRUE)
+  threshold_non_inf <- quantile(MRE, prob = 0.925, na.rm = TRUE)
   index_noise <- which(abs(MRE)>threshold_non_inf)
   
   # diagnostic plot
-  plot_df <- data.frame(cbind(wn = as.numeric(colnames(X_orig)), mre = MRE))
+  wn <- as.numeric(gsub("_", ".", 
+                        gsub("x", "", 
+                             colnames(X[, const_cols]))))
+  plot_df <- data.frame(cbind(wn = wn, mre = MRE))
   diag_plot <- ggplot(data = plot_df, aes(x = wn, y = mre)) +
     theme_minimal() +
     theme(panel.grid.major = element_line(),panel.grid.minor = element_blank(),
           panel.border = element_rect(fill=NA)) +
     geom_line() +
-    geom_hline(yintercept = threshold, linetype="dashed", color="red") +
+    geom_hline(yintercept = threshold_non_inf, linetype="dashed", color="red") +
     scale_x_reverse() +
     xlab(expression(paste("Wavenumber in ", bold(cm ^-1)))) +
     ylab("Mean reconstruction error")
-
+  
   show(diag_plot)
-  return(list(index = index_noise, X = Xhat))
+  
+  return(list(index = colnames(X[, const_cols])[index_noise], X = Xhat))
 }
